2675bff836a772b8998ba747ba0f04cf467be248
Anish Mudide <amudide@gmail.com>
2024-01-31 10:22:34 -0500

    Fix background scroll?

--- client/src/components/pages/styles.js
+++ client/src/components/pages/styles.js
@@ -2,13 +2,14 @@ import grid from "../../components/img/grid.svg";
 
 const bgStyle = {
   backgroundImage: `url(${grid})`,
-  // height: "100vh",
-  // width: "100vw",
   backgroundSize: "cover",
   backgroundPosition: "center",
+  backgroundAttachment: "fixed", // This will keep the background fixed during scrolling
+  backgroundRepeat: "no-repeat", // This ensures the image doesn't repeat
   width: "100vw",
-  height: "100vh",
+  minHeight: "100vh", // Ensures minimum height is the full viewport height
 };
+
 
 const buttonClass =
   "text-white bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-700 hover:to-purple-700 py-2 px-4 rounded shadow-md hover:shadow-lg transition duration-200 border-2 border-emerald-900 border-opacity-50";
