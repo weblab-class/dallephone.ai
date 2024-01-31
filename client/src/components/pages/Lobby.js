adb74adf09f64b320cecd0251fdb67ba52274b78
Anish Mudide <amudide@gmail.com>
2024-01-30 19:46:50 -0500

    Make title clickable to home screen

--- client/src/components/pages/Lobby.js
+++ client/src/components/pages/Lobby.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import OriginalPrompt from "./OriginalPrompt.js";
 import { socket } from "../../client-socket.js";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { get, post } from "../../utilities";
 import { bgStyle, buttonClass } from "./styles.js";
 
@@ -172,7 +172,7 @@ const Lobby = () => {
   return (
     <div style={bgStyle} className="flex flex-col justify-center items-center h-screen w-screen">
       <div style={headerStyle}>
-        <span style={{ fontSize: "56px", marginTop: "100px" }}>DALL-E Phone</span>
+        <Link to="/" style={{ fontSize: "56px", marginTop: "100px" }}>DALL-E Phone</Link>
       </div>
       <div style={containerStyle}>
         <div style={divStyle}>Game ID: {game_id}</div>
